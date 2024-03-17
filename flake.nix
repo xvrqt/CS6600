727c63653e78796418ce072a82c48f567601ec91
xvrqt <git@xvrqt.com>
2024-03-17 15:16:24 -0700

    It works!

--- flake.nix
+++ flake.nix
@@ -1,52 +1,91 @@
 {
-    description = "cs6600";
+  description = "Projects for the University of Utah's CS6600 Computer Graphics Course";
 
-    inputs = {
-        cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-        flake-utils.url = "github:numtide/flake-utils";
-        rust-overlay.url = "github:oxalica/rust-overlay";
-        rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-        rust-overlay.inputs.flake-utils.follows = "flake-utils";
-        nixpkgs.url = "github:nixos/nixpkgs";
-        flake-compat = {
-            url = "github:edolstra/flake-compat";
-            flake = false;
-        };
+  inputs = {
+    # No need to live dangerously
+    nixpkgs.url = "github:nixos/nixpkgs";
+
+    # Helps us package for every type of system
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # ???
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
 
-    outputs = inputs: with inputs;
+    # Helps us package Rust programs using Nix
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
 
-        flake-utils.lib.eachDefaultSystem (system:
-            let 
-                package_name = "cs6600";
-                pkgs = import nixpkgs {
-                    inherit system;
-                    overlays = [
-                            cargo2nix.overlays.default
-                        ];
-                };
+    # More Up-to-date Version of Rust
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
 
-                rustPkgs = pkgs.rustBuilder.makePackageSet {
-                    rustVersion = "1.61.0";
-                    packageFun = import ./Cargo.nix;
-                    extraRustComponents = ["clippy"];
-                };
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          package_name = "cs6600";
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              cargo2nix.overlays.default
+            ];
+          };
 
-                workspaceShell = rustPkgs.workspaceShell {
-                    packages = with pkgs; [
-                        rustfmt
-                        cargo2nix.packages.${system}.cargo2nix
-                    ];
-                };
+          # Need GLFW & OpenGL to Build + Link
+          buildInputs = with pkgs; [
+            libGL
+            wlr-protocols
+            wayland-protocols
+            extra-cmake-modules
+            glfw-wayland
+            wayland
+            stdenv
+            cmake
+            pkg-config
+            libxkbcommon
+            libglvnd
+          ];
 
-                ci = pkgs.rustBuilder.runTests rustPkgs.workspace.cargo2nix {};
+          nativeBuildInputs = with pkgs; [
+            # font-config
+          ];
 
-            in rec {
-                packages = {
-                    cs6600 = (rustPkgs.workspace.cs6600 {}).bin;
-                    default = packages.cs6600;
-                };
-                devShell = workspaceShell;
-            }
-        );
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            rustChannel = "stable";
+            rustVersion = "1.70.0";
+            packageFun = import ./Cargo.nix;
+            extraRustComponents = ["clippy"];
+          };
+
+          workspaceShell = rustPkgs.workspaceShell {
+            packages = with pkgs;
+              [
+                rustfmt
+                cargo2nix.packages.${system}.cargo2nix
+              ]
+              ++ buildInputs
+              ++ nativeBuildInputs;
+
+            buildInputs = buildInputs;
+            nativeBuildInputs = nativeBuildInputs;
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+          };
+
+          ci = pkgs.rustBuilder.runTests rustPkgs.workspace.cargo2nix {};
+        in rec {
+          packages = {
+            cs6600 = (rustPkgs.workspace.cs6600 {}).bin;
+            default = packages.cs6600;
+          };
+          devShell = workspaceShell;
+        }
+      );
 }
