39c8f92168d7534b8756da973df1cb8d4730fb44
xvrqt <git@xvrqt.com>
2024-03-17 14:23:50 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+    description = "cs6600";
+
+    inputs = {
+        cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+        flake-utils.url = "github:numtide/flake-utils";
+        rust-overlay.url = "github:oxalica/rust-overlay";
+        rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+        rust-overlay.inputs.flake-utils.follows = "flake-utils";
+        nixpkgs.url = "github:nixos/nixpkgs";
+        flake-compat = {
+            url = "github:edolstra/flake-compat";
+            flake = false;
+        };
+    };
+
+    outputs = inputs: with inputs;
+
+        flake-utils.lib.eachDefaultSystem (system:
+            let 
+                package_name = "cs6600";
+                pkgs = import nixpkgs {
+                    inherit system;
+                    overlays = [
+                            cargo2nix.overlays.default
+                        ];
+                };
+
+                rustPkgs = pkgs.rustBuilder.makePackageSet {
+                    rustVersion = "1.61.0";
+                    packageFun = import ./Cargo.nix;
+                    extraRustComponents = ["clippy"];
+                };
+
+                workspaceShell = rustPkgs.workspaceShell {
+                    packages = with pkgs; [
+                        rustfmt
+                        cargo2nix.packages.${system}.cargo2nix
+                    ];
+                };
+
+                ci = pkgs.rustBuilder.runTests rustPkgs.workspace.cargo2nix {};
+
+            in rec {
+                packages = {
+                    cs6600 = (rustPkgs.workspace.cs6600 {}).bin;
+                    default = packages.cs6600;
+                };
+                devShell = workspaceShell;
+            }
+        );
+}
